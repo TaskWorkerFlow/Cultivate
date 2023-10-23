6cda9215d77a41dd83b46ca0cabbb427e7453dda
Physeter <116091iyh@gmail.com>
2023-10-23 16:53:37 +0800

    添加antdv框架

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,55 @@
 <template>
-   <!-- 路由出口 -->
-  <!-- 路由匹配到的组件将渲染在这里 -->
-  <router-view></router-view>
+  <a-space direction="vertical" :style="{ width: '100%' }" :size="[0, 48]">
+    <a-layout>
+      <a-layout-header :style="headerStyle">Header</a-layout-header>
+      <a-layout>
+        <a-layout-sider :style="siderStyle">Sider</a-layout-sider>
+        <a-layout-content :style="contentStyle">
+          <!-- 路由出口 -->
+          <!-- 路由匹配到的组件将渲染在这里 -->
+          <router-view></router-view>
+        </a-layout-content>
+      </a-layout>
+      <a-layout-footer :style="footerStyle">Footer</a-layout-footer>
+    </a-layout>
+  </a-space>
 </template>
+<script lang="ts" setup>
+import type { CSSProperties } from "vue";
+const headerStyle: CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 50,
+  lineHeight: "64px",
+  backgroundColor: "#7dbcea"
+};
+
+const contentStyle: CSSProperties = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#108ee9"
+};
+
+const siderStyle: CSSProperties = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#3ba0e9"
+};
+
+const footerStyle: CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#7dbcea"
+};
+</script>
+<style>
+#app {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
--- src/views/taskList/taskList.vue
+++ src/views/taskList/taskList.vue
@@ -2,7 +2,7 @@
   <h2>任务列表</h2>
   <div>
     <ul>
-      <li v-for="item in task.taskList" :key="item.id">
+      <li v-for="item in taskList" :key="item.id">
         <div class="name">{{ item.name }}</div>
         <div class="desc">{{ item.taskDesc }}</div>
       </li>
@@ -10,9 +10,17 @@
   </div>
 </template>
 <script setup lang="ts">
+import { ref, reactive, watch, onMounted } from "vue";
 import { task } from "../../model/task";
 
-const taskList = task.taskList = [
+let taskList = ref<task.taskList>([]);
+watch(
+  taskList,
+  () => {
+    console.log("taskList变化了");
+  }
+);
+taskList.value = [
   {
     id: "22",
     name: "收集通过搜索引擎获取的图片",
@@ -22,4 +30,19 @@ const taskList = task.taskList = [
     taskTag: task.taskTag.urgent
   }
 ];
+
+onMounted(() => {
+  console.log(26, taskList);
+  setTimeout(() => {
+    console.log(111);
+    taskList.value.push({
+      id: "23",
+      name: "收集通过门户网站获取的图片",
+      taskDesc: "整理来自门户网站获取的图片",
+      taskType: task.taskType.imageCollection,
+      taskStatus: task.taskStatus.noStart,
+      taskTag: task.taskTag.urgent
+    });
+  }, 2000);
+});
 </script>
